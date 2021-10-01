13868077e5457079f6405da30a1f9660d3f0acf8
Darnell Davy <darnelldavy@darnells-mbp.myfiosgateway.com>
2021-10-01 19:42:06 -0400

    validated confirm

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,55 +14,75 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var uppercase = confirm("Do you want to use uppercase letters?")
-  console.log (uppercase)
 
   var uppercaseLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
-  var totalCharacters = "";
-
-  if (uppercase === true) {
-    totalCharacters = totalCharacters.concat(uppercaseLetters) 
-    console.log(totalCharacters)
-  }
-
-  var lowercase = confirm("Do you want to use lowercase letters?")
-
+  var totalCharacters = ""; 
   var lowercaseLetters = "qwertyuiopasdfghjklzxcvbnm";
-
-  if (lowercase === true) {
-    totalCharacters = totalCharacters.concat(lowercaseLetters) 
-    console.log(totalCharacters)
-  }
-
-  var numbers = confirm("Do you want to use numbers?")
-
   var numbersString = "1234567890";
-
-  if (numbers === true) {
-    totalCharacters = totalCharacters.concat(numbersString) 
-    console.log(totalCharacters)
-  }
-
-  var specialChar = confirm("Do you want to use a special character?")
-
   var specialCharString = "~!@#$%^&*()_+`{}|:<>?[];',./";
+  
+  function findCharacters () {
+    var uppercase = confirm("Do you want to use uppercase letters?")
+    var lowercase = confirm("Do you want to use lowercase letters?");
+    var numbers = confirm("Do you want to use numbers?");
+    var specialChar = confirm("Do you want to use a special character?")
+    
+    if (uppercase === true) {
+      totalCharacters = totalCharacters.concat(uppercaseLetters) 
+      console.log(totalCharacters)
+    }
 
-  if (specialChar === true) {
-    totalCharacters = totalCharacters.concat(specialCharString) 
-    console.log(totalCharacters)
+    if (lowercase === true) {
+      totalCharacters = totalCharacters.concat(lowercaseLetters) 
+      console.log(totalCharacters)
+    }
+
+    if (numbers === true) {
+      totalCharacters = totalCharacters.concat(numbersString) 
+      console.log(totalCharacters)
+    }
+  
+    if (specialChar === true) {
+      totalCharacters = totalCharacters.concat(specialCharString) 
+      console.log(totalCharacters)
+    }
   }
 
-  var passLength = prompt("Please choose a password length between 8 - 128 characters")
+  findCharacters()
+
+  if (totalCharacters.length === 0) {
+    findCharacters()
+  }
+
+var passLength;
+function getLength(){
+  passLength = prompt("Please choose a password length between 8 - 128 characters")
+return passLength
+}
+getLength()
 
   if (passLength < 8 || passLength > 128) {
-    window.prompt ("Choose a password BETWEEN 8 - 128 characters")
+    window.alert("Choose a password BETWEEN 8 - 128 characters")
+    getLength()
   }
 
   if (isNaN(passLength)) {
-    window.prompt ("Choose only numbers")
+    window.alert("Choose only numbers")
+    getLength()
   }
+  console.log("password length = " + passLength)
 }
 
+
+
+
+//WHAT WE HAVE
+//what the password is made of (numbers, letters, etc)
+//know how long it should be
+//WHAT WE NEED
+//you need to use a for loop, and you need to iterate through the totalCharacters var as many times as the password is long
+//choose a random character 17 times (Math.random)
+//as it chooses a random character, you need to PUSH that character to the final password
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
